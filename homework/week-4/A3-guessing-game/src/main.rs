6e06851fe0b4de8f2064c6bf5d8a3edf624c7c3e
Iris Seaman <mirisr@users.noreply.github.com>
2024-10-07 14:20:13 +0000

    updating partial hw

--- homework/week-4/A3-guessing-game/src/main.rs
+++ homework/week-4/A3-guessing-game/src/main.rs
@@ -1,3 +1,32 @@
+use std::io;
+
+
+fn check_guess(guess: i32, secret: i32) -> i32 {
+    if guess > secret {
+        return 1;
+    }
+    if guess < secret {
+        return -1;
+    }
+    return 0;
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut secret = 10;
+
+    let mut guess = 0;
+    let mut input = String::new();
+
+    while guess != secret {
+
+        input.clear();
+        println!("Guess my secret number:");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        let guess: i32 = input.trim().parse().expect("Not a valid number");
+
+    }
 }
